fe6706cdb759993eb84fe49038bcf1b3cac2c4f1
artem <bakirovartem69@gmail.com>
2022-07-14 14:22:38 +0200

    initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,25 @@
+// This is a single line JavaScript comment
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+const helloWorld = "Hello World";
+let pi = 3.14;
+const burjDubai = "828m";
+let fullName = "Jan Schmidt";
+const eifelTower = "324m";
+const camelCase = "camelCase";
+let kursStatus = true;
+
+function myFunction() {
+  document.getElementById("test").style.color = "red";
+}
